8f8ae3528d7da86a909854af84591fec7bc77d25
Mosaab <musab.ehab@gmail.com>
2023-02-10 09:13:02 +0200

    add search request and tests for the client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,26 +7,14 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
+	"io"
 	"log"
 	"pcbook/pb"
 	"pcbook/sample"
 	"time"
 )
 
-func main() {
-	serverAddress := flag.String("address", "", "the server address")
-	flag.Parse()
-
-	log.Printf("dial server: %s", *serverAddress)
-
-	conn, err := grpc.Dial(*serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	if err != nil {
-		log.Fatal("cannot dial server: ", err)
-	}
-
-	laptopClient := pb.NewLaptopServiceClient(conn)
-
+func createLaptop(laptopClient pb.LaptopServiceClient) {
 	laptop := sample.Newlaptop()
 
 	req := &pb.CreateLaptopRequest{
@@ -49,4 +37,71 @@ func main() {
 	}
 
 	log.Printf("Created Laptop with id: %s", res.Id)
+}
+
+func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
+	log.Print("search filter", filter)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &pb.SearchLaptopRequest{
+		Filter: filter,
+	}
+	stream, err := laptopClient.SearchLaptop(ctx, req)
+	//log.Println(stream)
+	if err != nil {
+		log.Fatal("can not search laptop:", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal("cannot receive response: ", err)
+		}
+
+		laptop := res.GetLaptop()
+		log.Println("- found: ", laptop.GetId())
+		log.Println(" 	+ brand: ", laptop.GetBrand())
+		log.Println(" 	+ name: ", laptop.GetName())
+		log.Println(" 	+ cpu cores: ", laptop.GetCpu().GetNumberCores())
+		log.Println(" 	+ min ghz: ", laptop.GetCpu().GetMinGhz())
+		log.Println(" 	+ max ghz: ", laptop.GetCpu().GetMaxGhz())
+		log.Println(" 	+ ram: ", laptop.GetMemory().GetValue(), laptop.GetMemory().GetUnit())
+		log.Println(" 	+ price: ", laptop.GetPriceUsd(), "usd")
+	}
+}
+
+func main() {
+	serverAddress := flag.String("address", "", "the server address")
+	flag.Parse()
+
+	log.Printf("dial server: %s", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+
+	for i := 0; i < 10; i++ {
+		createLaptop(laptopClient)
+	}
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 3000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.5,
+		MinRam: &pb.Memory{
+			Value: 8,
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+
+	searchLaptop(laptopClient, filter)
 }
--- service/laptop_client_test.go
+++ service/laptop_client_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"net"
 	"pcbook/pb"
 	"pcbook/sample"
@@ -15,7 +16,7 @@ import (
 func TestClientCreateLaptop(t *testing.T) {
 	t.Parallel()
 
-	laptopServer, serverAddress := startTestLaptopServer(t)
+	laptopServer, serverAddress := startTestLaptopServer(t, NewInMemoryLaptopStore())
 	laptopClient := newTestLaptopClient(t, serverAddress)
 
 	laptop := sample.Newlaptop()
@@ -45,8 +46,8 @@ func newTestLaptopClient(t *testing.T, address string) pb.LaptopServiceClient {
 	return pb.NewLaptopServiceClient(conn)
 }
 
-func startTestLaptopServer(t *testing.T) (*LaptopServer, string) {
-	laptopServer := NewLaptopServer(NewInMemoryLaptopStore())
+func startTestLaptopServer(t *testing.T, store LaptopStore) (*LaptopServer, string) {
+	laptopServer := NewLaptopServer(store)
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
@@ -69,4 +70,85 @@ func requireSameLaptop(t *testing.T, laptop1, laptop2 *pb.Laptop) {
 	require.NoError(t, err)
 
 	require.Equal(t, json1, json2)
+}
+
+func TestClientSearchLaptop(t *testing.T) {
+	t.Parallel()
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 2000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.2,
+		MinRam: &pb.Memory{
+			Value: 8,
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+
+	store := NewInMemoryLaptopStore()
+	expectedIds := make(map[string]bool)
+
+	for i := 0; i < 6; i++ {
+		laptop := sample.Newlaptop()
+
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Memory = &pb.Memory{
+				Value: 4096,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Memory = &pb.Memory{
+				Value: 16,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.GetId()] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 5
+			laptop.Cpu.MinGhz = 2.8
+			laptop.Cpu.MaxGhz = 5.0
+			laptop.Memory = &pb.Memory{
+				Value: 64,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.GetId()] = true
+		}
+
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	_, serverAddress := startTestLaptopServer(t, store)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+
+	require.NoError(t, err)
+
+	found := 0
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		require.NoError(t, err)
+		require.Contains(t, expectedIds, res.GetLaptop().GetId())
+		found += 1
+	}
+
+	require.Equal(t, len(expectedIds), found)
 }
